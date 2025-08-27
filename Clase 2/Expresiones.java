64f4c6a41042372c97b58232af16cccbc776c2d7
Stiven <yesid.balanta@campusucc.edu.co>
2025-08-27 20:52:38 +0000

    update

---
+++ Clase 2/Expresiones.java
@@ -0,0 +1,25 @@
+public class Expresiones {
+
+    public static void main(String[] args) {
+        
+        int a = 2;
+        int b = 5;
+        int c = 0;
+        c = 3 *a - 4 * b / (a * a);
+
+        double d = 0;
+        d = 4 / 2 * 3 / 6 + 6 / 2 / 1 / Math.pow(5, 2) / 4 * 2;
+
+        System.out.println("El resultado es: " + c);
+        System.out.println("El resultado es: " + d);
+
+        int A = 4;
+        int B = 5;
+        int C = 1;
+
+        System.out.println(B * A - Math.pow(5, 2)/ 4 * C);
+        System.out.println((A * B) / Math.pow(3, 2));
+        System.out.println( (((B + C) / 2 * A + 10) * 3 * B) - 6);
+    }
+
+}
