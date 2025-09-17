069f2d441a15f8180628485680957f240ca4bafb
Stiven <yesid.balanta@campusucc.edu.co>
2025-09-17 21:53:15 +0000

    update

--- Clase 7/Persona.java
+++ Clase 7/Persona.java
@@ -8,11 +8,21 @@ public class Persona{
     private double altura;
     private double peso;
     
+    //Metodo constructor de la clase se reconoce por que tiene el mismo nombre de la clase
+    //El constructor me permite inicializar la clase
+    //La palabra reservada "This" me permite referenciar los atributos de la clase
+    public Persona(int indentificacion, String nombres, String apellidos, int edad, double estatura, double peso){
+        this.indentificacion = indentificacion;
+        this.nombres = nombres;
+        this.apellido = apellidos;
+        this.edad = edad;
+        this.altura = estatura;
+        this.peso = peso;
+    }
     //Metodos
     public void caminar(){}
     public boolean jugar(){}
     public void comer(){}
     public String estudiar(){}
-    
 
 }
