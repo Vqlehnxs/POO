8e249836810bfb786b045b9778954435e5e55ecf
Stiven <yesid.balanta@campusucc.edu.co>
2025-11-05 20:17:38 +0000

    Cambios

--- Clase13/Taller/Tienda.java
+++ Clase13/Taller/Tienda.java
@@ -3,8 +3,9 @@ import java.util.Date;
 public class Tienda {
     public static void main(String[] args) {
         Cliente cliente = new Cliente(12345678, "María González");
+        Producto camara = new Camara(1, "Cámara Digital", 300, true, "Canon", "EOS 1500D", "24MP");
         Foto foto = new Foto("1", "JPG", 140);
-        Impresion impresion = new Impresion(1, "Impresión Premium", 45, true, "Color", foto, "A3");
+        Producto impresion = new Impresion(1, "Impresión Premium", 45, true, "Color", foto, "A3");
 
         Pedido pedido = new Pedido(cliente, impresion, new Date(), 9653456, foto);
 
